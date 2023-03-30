9f74bddd4e3c3d36b6cf295fbb2d4c8b732af3a0
vmathu <anhthuvominh1@gmail.com>
2023-03-30 11:51:32 +0700

    Implement genetic algorithm

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import heapq
+import random
+import numpy as np
 
 
 class Item:
@@ -59,7 +61,8 @@ class BranchAndBound:
     def __init__(self, problem: Problem):
         self.problem = problem
         self.sorted_item_list: list[Item] = []
-        self.class_oder: list[list[int]] = [[] for _ in range(problem.number_of_class)]
+        self.class_oder: list[list[int]] = [[]
+                                            for _ in range(problem.number_of_class)]
         self.get_sorted_item_list()
         '''
                 for i in range(problem.number_of_item):
@@ -70,7 +73,8 @@ class BranchAndBound:
         '''
 
     def get_sorted_item_list(self):
-        max_weight = max(self.problem.initial_item_list, key=lambda item: item.weight).weight
+        max_weight = max(self.problem.initial_item_list,
+                         key=lambda item: item.weight).weight
         self.sorted_item_list = sorted(self.problem.initial_item_list, reverse=True,
                                        key=lambda item: (item.ratio, max_weight - item.weight))
         for i in range(self.problem.number_of_item):
@@ -101,7 +105,8 @@ class BranchAndBound:
                 if i == 0:
                     return 100, 100
                 weight -= self.sorted_item_list[i].weight
-                cost += (self.problem.capacity-weight)*self.sorted_item_list[i].ratio
+                cost += (self.problem.capacity-weight) * \
+                    self.sorted_item_list[i].ratio
                 break
             upper_bound += self.sorted_item_list[i].value
             cost = upper_bound
@@ -120,7 +125,8 @@ class BranchAndBound:
             weight += self.sorted_item_list[i].weight
             if weight > self.problem.capacity:
                 weight -= self.sorted_item_list[i].weight
-                cost += (self.problem.capacity - weight) * self.sorted_item_list[i].ratio
+                cost += (self.problem.capacity - weight) * \
+                    self.sorted_item_list[i].ratio
                 break
             upper_bound += self.sorted_item_list[i].value
             cost = upper_bound
@@ -151,11 +157,13 @@ class BranchAndBound:
                     weight += self.sorted_item_list[i].weight
                     if weight > self.problem.capacity:
                         weight -= self.sorted_item_list[i].weight
-                        cost += (self.problem.capacity - weight) * self.sorted_item_list[i].ratio
+                        cost += (self.problem.capacity - weight) * \
+                            self.sorted_item_list[i].ratio
                         break
                     upper_bound += self.sorted_item_list[i].value
                     cost = upper_bound
-                    class_not_active.remove(self.sorted_item_list[i].class_item - 1)
+                    class_not_active.remove(
+                        self.sorted_item_list[i].class_item - 1)
                     tmp.append(i)
             if weight < self.problem.capacity:
                 for i in range(node.depth + 1, self.problem.number_of_item):
@@ -164,7 +172,8 @@ class BranchAndBound:
                     weight += self.sorted_item_list[i].weight
                     if weight > self.problem.capacity:
                         weight -= self.sorted_item_list[i].weight
-                        cost += (self.problem.capacity - weight) * self.sorted_item_list[i].ratio
+                        cost += (self.problem.capacity - weight) * \
+                            self.sorted_item_list[i].ratio
                         break
                     upper_bound += self.sorted_item_list[i].value
                     cost = upper_bound
@@ -173,7 +182,8 @@ class BranchAndBound:
                 weight += self.sorted_item_list[i].weight
                 if weight > self.problem.capacity:
                     weight -= self.sorted_item_list[i].weight
-                    cost += (self.problem.capacity - weight) * self.sorted_item_list[i].ratio
+                    cost += (self.problem.capacity - weight) * \
+                        self.sorted_item_list[i].ratio
                     break
                 upper_bound += self.sorted_item_list[i].value
                 cost = upper_bound
@@ -208,10 +218,13 @@ class BranchAndBound:
             tmp4 = curr.active_class.copy()
             tmp1.append(1)
             if self.sorted_item_list[curr.depth + 1].class_item - 1 not in tmp3:
-                tmp3.append(self.sorted_item_list[curr.depth + 1].class_item - 1)
+                tmp3.append(
+                    self.sorted_item_list[curr.depth + 1].class_item - 1)
             tmp2.append(0)
-            node1 = Node(curr.depth + 1, curr.current_weight, curr.current_value, tmp1, tmp3)
-            node2 = Node(curr.depth + 1, curr.current_weight, curr.current_value, tmp2, tmp4)
+            node1 = Node(curr.depth + 1, curr.current_weight,
+                         curr.current_value, tmp1, tmp3)
+            node2 = Node(curr.depth + 1, curr.current_weight,
+                         curr.current_value, tmp2, tmp4)
             l: list[Node] = []
             if self.check_node(node1, upper):
                 l.append(node1)
@@ -237,19 +250,110 @@ class BranchAndBound:
         for i in range(0, len(self.sorted_item_list)):
             self.sorted_item_list[i].state = max_item[i]
             # if max_item[i] == 1:
-                # res += self.sorted_item_list[i].weight
+            # res += self.sorted_item_list[i].weight
         # print(res)
+
+
+class GeneticAlgorithm:
+    def __init__(self, problem: Problem):
+        self.problem = problem
+        self.population = 700
+        self.N = problem.number_of_item
+
+    def random_population(self):
+        generation_list = []
+        for _ in range(self.population):
+            gene = []
+            for _ in range(self.N):
+                gene.append(random.randint(0, 1))
+            # last element save fitness value
+            gene.append(0)
+            generation_list.append(gene)
+        return generation_list
+
+    def fitness(self, population_list):
+        for i in range(len(population_list)):
+            w = 0
+            v = 0
+            temp = set()
+            for j in range(self.N):
+                if population_list[i][j]:
+                    w += p.initial_item_list[j].weight
+                    v += p.initial_item_list[j].value
+                    temp.add(p.initial_item_list[j].class_item)
+            population_list[i][self.N] = v if w <= p.capacity else 0
+            population_list[i][self.N] += 10**len(temp)
+        # sort
+        for i in range(len(population_list)-1):
+            for j in range(i+1, len(population_list)):
+                if (population_list[j][self.N] > population_list[i][self.N]):
+                    population_list[j], population_list[i] = population_list[i], population_list[j]
+        return population_list
+
+    def cross_over(self, generation_list):
+        for i in range(0, len(generation_list), 2):
+            new1 = generation_list[i][:self.N//2] + \
+                generation_list[i+1][self.N//2:]
+            new2 = generation_list[i+1][:self.N//2] + \
+                generation_list[i][self.N//2:]
+            new1[self.N] = 0
+            new2[self.N] = 0
+            generation_list.append(new1)
+            generation_list.append(new2)
+        return generation_list
+
+    def mutation(self, generation_list, population):
+        muted_list = []
+        for _ in range(population//2):
+            new_rand = random.randint(population//2, population-1)
+            if new_rand not in muted_list:
+                muted_list.append(new_rand)
+                generation_list[new_rand][random.randint(
+                    0, self.N-1)] = random.randint(0, 1)
+        return generation_list
+
+    def solve_problem(self):
+        current_generation = self.random_population()
+        current_generation = self.fitness(current_generation)
+        for _ in range(500):
+            current_generation = current_generation[0:self.population//2]
+            new_generation = self.cross_over(current_generation)
+            new_generation = self.mutation(new_generation, self.population)
+            current_generation = new_generation
+            current_generation = self.fitness(current_generation)
+        # print solution
+        ans = 0
+        for i in range(self.N):
+            item = p.initial_item_list[i]
+            item.state = current_generation[0][i]
+            if current_generation[0][i]:
+                ans += item.value
+        if current_generation[0][self.N]-10**self.problem.number_of_class == ans:
+            print(ans)
+        else:
+            print("No solution!")
 
 
 if __name__ == '__main__':
     input_file_txt = "testcase5.txt"
     # input_file_txt = "input1.txt"
     p = Problem(input_file_txt)
-    search1 = BranchAndBound(p)
-    search1.get_sorted_item_list()
-    n = Node(1, 2, 10, [1, 1], [])
+    opt = int(input(
+        "Enter number to choose algorithms: "))
+
+    if (opt == 1):
+        search = BranchAndBound(p)
+        search.get_sorted_item_list()
+        n = Node(1, 2, 10, [1, 1], [])
+    # elif (opt == 2):
+    #     solution = 
+    # elif (opt == 3):
+    #     solution = 
+    elif (opt == 4):
+        search = GeneticAlgorithm(p)
     s = ""
-    search1.solve_problem()
+    search.solve_problem()
+
     for i in range(len(p.initial_item_list)):
         if len(p.initial_item_list) - 1 == i:
             s += str(p.initial_item_list[i].state)
